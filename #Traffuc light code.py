3e2365c9524db136a045458fab66e7cdac9eb1d5
Your Name <mahiadit4321@gmail.com>
2026-05-26 23:07:00 +0530

    Add Python code files

---
+++ #Traffuc light code.py
@@ -0,0 +1,12 @@
+#Traffuc light code
+light = input("light: ")
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("go slow")
+elif(light=="green"):
+    print("begone")
+else:
+    print("light is broken")
+
+input("press enter to exit: ")
