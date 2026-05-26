cdd9f32a382f08eb23efb9356bc7052da4ee3d72
Your Name <mahiadit4321@gmail.com>
2026-05-26 22:22:42 +0530

    added day1

---
+++ conditional steps.py
@@ -0,0 +1,22 @@
+x = float(input("enter the number: "))
+if x < 2:
+    print("smaller")
+elif x < 5:
+    print("medium")
+elif x < 10:
+    print("big")
+elif x < 20:
+    print("large")
+else :
+    print("gigantic")
+
+
+
+#conditional statements
+x = float(input("enter your number: "))
+if x < 2 :
+    print("small")
+elif x < 10:
+    print("medium")
+else:
+    print('something else')
