8e3b597ab57273b6c7f6dbfb7a230d0e5be048bc
Your Name <mahiadit4321@gmail.com>
2026-05-26 23:09:12 +0530

    Add simple interest calculator

---
+++ simple-interest-calculator.py
@@ -0,0 +1,7 @@
+#simple interset calculator
+P = float(input("principal: "))
+R = float(input("rate: "))
+T = float (input("time: "))
+
+print(P*R*T/100,"is your simple interest")
+input("press enter to end...")
