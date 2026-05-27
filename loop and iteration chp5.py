1c01d4d5caa983a5422102d973e74395c05d9ba0
Your Name <mahiadit4321@gmail.com>
2026-05-28 04:06:09 +0530

    some improvements

--- loop and iteration chp5.py
+++ loop and iteration chp5.py
@@ -1,20 +1,18 @@
+#learning loops
 
+num = float(input("Enter the decend number: "))
+n = num
+while n > 0:  
+  print("blast off")
 
-# num = float(input("Enter the decend number: "))
-
-
-# n = num
-# while n > 0:  
-#   print("blast off")
-
-
-# while True:
-#     line = input('> ')
-#     if line[0] == '#':
-#         continue
-#     if line == 'done':
-#         break
-#     print(line)
-# print("done")
+#while loop
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line == 'done':
+        break
+    print(line)
+print("done")
 
 
