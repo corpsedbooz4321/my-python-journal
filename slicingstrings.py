39885f89c8c5a79511336095f0f9bc717fcf6f5f
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-06-02 13:09:05 +0000

    slicing strings practice code

---
+++ slicingstrings.py
@@ -0,0 +1,30 @@
+'''we can also look at any continuous sectionof a string using a colon operator
+the second number is one beyond the end of the slilce up to but not the including.
+if the second number is beyond the end od the string it stops at the end.'''
+
+# word = 'Monty python'
+# print(word[0:5])
+
+'''When the  + operator is applied to strings , it means "concatenation"'''
+# a = 'Hello'
+# b = a + ' ' + 'There'
+# print(b)
+
+'''Using in as a logical opertor'''
+'''The in keyword can also be used to check to see if one string is "in" another string.
+The in expression is a logical expression that returns True or false and can be used in an if statement.'''
+
+# if word == 'banana':
+#     print('All right, bananas.')
+# if word < 'banana':
+#     print('your word,' + word + ',comes before banana.')
+# elif word > 'banana':
+#     print('your word,' + word + ', comes agter banana.')
+# else:
+#     print('All right , banana.')
+
+
+'''string library'''
+greet = 'hello boby'
+tap = greet.lower()
+print(tap)
