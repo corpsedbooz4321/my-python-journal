dd9fd573cd302acd7468c580d1c7113921a56cee
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-06-02 09:32:11 +0000

    strings again

---
+++ chp6 strings.py
@@ -0,0 +1,12 @@
+# fruit = 'banana'
+# letter = fruit[3]
+# print(letter)
+# print(len(fruit))
+
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
+ 
