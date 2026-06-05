aece3778bd1f2458ab7890603012bc9eb4608a22
adityasharma <mahiadit4321@gmail.com>
2026-06-06 00:14:23 +0530

    more methods

--- chapter_4/02_list_method.py
+++ chapter_4/02_list_method.py
@@ -1,1 +1,19 @@
 friends = ["Apple", "orange", 4, 534.04, False, "Akash", "Rohan"]
+
+print(friends)
+friends.append("sharma")
+print(friends)
+
+
+l1 = [34, 54, 55, 484, 94]
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+
+l1.insert(4, 48475939)
+print(l1)
+
+l1.pop()
