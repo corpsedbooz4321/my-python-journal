1ef858cbb50212af39ce7d443c9a8962b4a98918
adityasharma <mahiadit4321@gmail.com>
2026-06-05 16:19:39 +0530

    more strings

--- chp6 strings.py
+++ chp6 strings.py
@@ -1,24 +1,32 @@
-fruit = 'banana'
-letter = fruit[3]
-print(letter)
-print(len(fruit))
+# fruit = 'banana'
+# letter = fruit[3]
+# print(letter)
+# print(len(fruit))
 
-fruit = 'banana'
-index = 0
-while index < len(fruit):
-    letter = fruit[index]
-    print(index, letter)
-    index = index + 1
+# fruit = 'banana'
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(index, letter)
+#     index = index + 1
  
 
-fruit = '145434323465'
-for letter in fruit:
-    print(letter)
+# fruit = '145434323465'
+# for letter in fruit:
+#     print(letter)
     
 
-word = 'adityasharma'
-count = 0
-for letter in word:
-    if letter == 'a':
-        count = count + 1
-print(count)
+# word = 'adityasharma'
+# count = 0
+# for letter in word:
+#     if letter == 'a':
+#         count = count + 1
+# print(count)
+
+
+
+name = 'Adiyta'
+
+print(name[0:4])
+
+print(name[-3:-1])
