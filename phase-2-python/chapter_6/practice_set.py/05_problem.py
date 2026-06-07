dad30c4d29bc9f823fbc1210c9827e8cd3293df7
adityasharma <mahiadit4321@gmail.com>
2026-06-08 00:08:22 +0530

    add: implement name presence check and list update functionality

---
+++ phase-2-python/chapter_6/practice_set.py/05_problem.py
@@ -0,0 +1,33 @@
+'''write a program that finds out whether the given name is present in a list or not'''
+
+l = ["happy", "rohan", "aditya", "rahul", "sujal"]
+# print(type(l))
+def name_check():
+    name = input("Enter your name: ").lower()
+
+    if name in l:
+        print("The given name is already present in the list!")
+    else:
+        print("The given name is not present in the list!")
+def list_update():
+    name1 = input("Enter a name to add: ").lower()
+    l.append(name1)
+    print(f"{name1} has been added to the list!")
+
+def menu():
+    print("(y) to check, if the name is present in the list or not.")
+    print("(n) to update or add a name in the list.")
+    print("(q) to exit.")
+menu()
+
+while True:
+    choice = (input("Choose: ")).lower()
+    if choice == "y":
+        name_check()
+    elif choice == "n":
+        list_update()
+    elif choice == "q":
+        print("Goodbye!")
+        break
+    else:
+        print("invalid choice!")
