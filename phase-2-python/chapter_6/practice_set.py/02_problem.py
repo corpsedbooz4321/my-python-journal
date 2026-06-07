2913a1ea1c5bc7b9d9a98da806c02e18a854078d
adityasharma <mahiadit4321@gmail.com>
2026-06-07 22:41:29 +0530

    fix: correct comments and improve student pass/fail program logic

--- phase-2-python/chapter_6/practice_set.py/02_problem.py
+++ phase-2-python/chapter_6/practice_set.py/02_problem.py
@@ -1,13 +1,13 @@
 '''write a program to find out whether a student ahs passed or failedkif it requires a fotal of 4o% and at least 33% in each subject to pass. assume 3 subjects
 and take marks as an iput form the user'''
 
-def check_result(mk, mp, mc):
-    total_percentage = ((mk + mp + mc) / 300) * 100
+# def check_result(mk, mp, mc):
+#     total_percentage = ((mk + mp + mc) / 300) * 100
     
-    if mk >= 33 and mp >= 33 and mc >= 33 and total_percentage >= 40:
-        return True, total_percentage
-    else:
-        return False, total_percentage
+#     if mk >= 33 and mp >= 33 and mc >= 33 and total_percentage >= 40:
+#         return True, total_percentage
+#     else:
+#         return False, total_percentage
 
 
 mk = int(input("Enter the marks of first subject : "))
@@ -25,3 +25,15 @@ else:
         print(f"Sorry, you failed. Total percentage: {percentage:.2f}%")
 
    
+
+marks1 = int(input("Enter your marks for subject 1: "))
+marks2 = int(input("Enter your marks for subject 2: "))
+marks3 = int(input("Enter your marks for subject 3: "))
+            
+#check for total percentage.
+total_percentage = (100*(marks1 + marks2 + marks3)) / 300
+
+if (total_percentage >= 40 and marks1 >= 33 and marks2 >= 33 and marks3 >= 33):
+    print("you passed,your total percentage is", total_percentage)
+else:
+    print("you failed, your total percentage is", total_percentage)
