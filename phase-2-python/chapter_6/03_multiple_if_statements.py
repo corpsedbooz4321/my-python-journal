85cf39df4482a3c592e2d0a46cfb79d417c4e5ab
adityasharma <mahiadit4321@gmail.com>
2026-06-07 16:59:04 +0530

    add age validation checks and even number identification

---
+++ phase-2-python/chapter_6/03_multiple_if_statements.py
@@ -0,0 +1,20 @@
+a = int(input("Enter age: "))
+
+#if staatement no. 1
+if (a%2 == 0):
+    print("a is even")
+#end of statement no.1
+
+#if statement no. 2
+if a >= 18:
+    print("you are above the age of consent")
+    print("GOOD for you")
+elif a < 0:
+    print("you are entering an inavlid negative age")
+elif(a==0):
+    print("you are enteringa 0 which is not a valid age")
+else:
+    print("you are below the age of consent")
+
+print("end of the program")
+#end of statement no. 2
