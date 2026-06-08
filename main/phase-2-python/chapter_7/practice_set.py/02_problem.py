45450ebbad89f2f95aa7d2a824a09a03187798df
adityasharma <mahiadit4321@gmail.com>
2026-06-08 20:32:25 +0530

    Add program to greet names starting with 'S' from a list

---
+++ main/phase-2-python/chapter_7/practice_set.py/02_problem.py
@@ -0,0 +1,7 @@
+'''write a program to greet all the persons name stored in a list "i" and which starts with S.'''
+
+l = ["aditya", "soham", "sachin","rahul"]
+
+for i in l:
+    if (i.startswith("s")):   
+        print(f"Hello, and welcome, {i}")
