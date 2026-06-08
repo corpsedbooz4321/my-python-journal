64e56192e16e3b73abf8ea7a9f04ea2fc00c22e9
adityasharma <mahiadit4321@gmail.com>
2026-06-08 19:31:57 +0530

    Add example of for loop with else clause

---
+++ main/phase-2-python/chapter_7/06_for_loop_with_else.py
@@ -0,0 +1,15 @@
+'''an optional else can be used with a for loop if the code i sto be executed when the loops exhausts'''
+
+
+#example
+l = [1, 7, 8]
+for item in l:
+    print(item)
+else:
+    print("done")
+
+'''*output*'''
+''''1
+    7
+    8
+    done'''
