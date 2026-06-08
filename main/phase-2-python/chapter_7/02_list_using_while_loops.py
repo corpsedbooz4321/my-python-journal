a1e4fd70b115f42259d4b77925641ed655a82fcc
adityasharma <mahiadit4321@gmail.com>
2026-06-08 16:31:00 +0530

    Add program to print list contents using while loops

---
+++ main/phase-2-python/chapter_7/02_list_using_while_loops.py
@@ -0,0 +1,10 @@
+'''write a program to print the content of a list using while looops'''
+
+
+l = (475, 'aditya', "8756", "shubham", "rahul", "tanya", 90)
+
+i = 0
+
+while (i < len(l)):
+    print(l[i])
+    i += 1
