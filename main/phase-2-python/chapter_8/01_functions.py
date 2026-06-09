a8448647dc9cdd22ccb17bf3208d4bdcaea62eaf
adityasharma <mahiadit4321@gmail.com>
2026-06-10 00:27:04 +0530

    Refactor code by commenting out unused variables and enhancing function documentation

--- main/phase-2-python/chapter_8/01_functions.py
+++ main/phase-2-python/chapter_8/01_functions.py
@@ -1,6 +1,34 @@
-a = 12
+# a = 12
 
-b = 45
+# b = 45
 
-c = 56
+# c = 56
+
+# average = (a + b + c) / 3
+
+# print(average)
+
+
+'''a function is a group of statements performing a specific task
+
+when a program gets bigger in size and its completly grows. it gets difficult for a program to keep track 
+on which piece of code is doing what
+
+
+a fuction can be reused by the program  in a given program any number of times'''
+
+
+def cal():
+    xx = int(input("Enter the number of times you want to print your name: "))
+    xxx = input("Enter your name:  ")
+    
+    print((xx * xxx), end=" ")
+cal()
+              
+
+
+
+
+
+
 
