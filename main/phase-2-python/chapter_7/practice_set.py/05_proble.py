350189647ebd5c50f21cd5390ff29272f0cb54e9
adityasharma <mahiadit4321@gmail.com>
2026-06-09 21:08:25 +0530

    Add program to find the sum of first natural numbers using while loop

---
+++ main/phase-2-python/chapter_7/practice_set.py/05_proble.py
@@ -0,0 +1,14 @@
+'''write a program to find the sum of first natural numbers using while loop?0'''
+
+
+n = int(input("Enter the number: "))
+
+i = 1
+
+sum = 0
+
+while(i<=n):
+    sum += i
+    i += 1
+
+print(sum)
