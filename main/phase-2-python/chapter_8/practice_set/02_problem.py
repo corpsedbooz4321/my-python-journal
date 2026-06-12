844d7d1f55f5e62d6b9e070018c7dd9b7b23f612
adityasharma <mahiadit4321@gmail.com>
2026-06-13 02:52:22 +0530

    Add function to convert Celsius to Fahrenheit

---
+++ main/phase-2-python/chapter_8/practice_set/02_problem.py
@@ -0,0 +1,13 @@
+'''Write a python program using function to convert celsius to 
+Fahrenheit.'''
+
+def celsius_to_fahrenheit(a):
+    fahrenheit = (a * 1.8) + 32
+    return fahrenheit
+
+a = float(input("Enter your celsius number: "))
+
+
+fahrenheit = celsius_to_fahrenheit(a)
+
+print(f"The the temperature in fahrenheit is {fahrenheit}")
