35989774dd85e1f46f7b7ebb6d341b5ab78e30e9
adityasharma <mahiadit4321@gmail.com>
2026-06-22 01:06:49 +0530

    Add file_write.py with example of list manipulation and file writing

---
+++ main/python/phase-2-python/chapter_9/02_file_write.py
@@ -0,0 +1,15 @@
+val = """friends = ["Apple", "orange", 4, 534.04, False, "Akash", "Rohan"]
+
+
+print(friends[0]) #unlike strings lists are mutable
+friends[0] = "Mango"
+
+print(friends[0])
+
+print(friends[1:5])"""
+
+
+f = open("file.txt", "w")
+f.write(val)
+
+f.close()
