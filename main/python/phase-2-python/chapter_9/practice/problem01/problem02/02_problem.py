bb9d17736846254f3324b024cc361c489b876b20
adityasharma <mahiadit4321@gmail.com>
2026-06-22 03:10:53 +0530

    Fix formatting and improve high score file handling in game function

--- main/python/phase-2-python/chapter_9/practice/problem01/problem02/02_problem.py
+++ main/python/phase-2-python/chapter_9/practice/problem01/problem02/02_problem.py
@@ -1,23 +1,33 @@
 '''The game() function in a program lets a user play a game and returns the score as 
-an integer.you need to read a file "Hi-score.txt"
-which is either bland or contains the game() function breaks the Hi-score?'''
+an integer. You need to read a file "high_score.txt" which is either blank or contains the game high score.'''
 
 import random
+import os
+
+script = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script, "high_score.txt")
 
 def game():
-	print("You are playing the game...")
-	score = random.randint(1, 100)
-	with open("high_score.txt", "r") as f:
-		highscore = f.read()
-		if highscore != "":
-			highscore = int(highscore)
-		else:
-			highscore = 0
-	print(f"Your score: {score}")
-	if score > highscore:
-		#write this high score into a file!
-		with open("high_score.txt", "w") as f:
-			print(f.write(str(f"your high score is : {score}")))
+    print("You are playing the game...")
+    score = random.randint(1, 100)
+
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            highscore = f.read()
+        if highscore != "":
+            highscore = int(highscore)
+        else:
+            highscore = 0
+    else:
+        highscore = 0
+
+    print(f"Your score: {score}")
+    if score > highscore:
+        print("New High Score!")
+        # write this high score into a file!
+        with open(file_path, "w") as f:
+            f.write(str(score))
+    return score
 game()
 	
 
