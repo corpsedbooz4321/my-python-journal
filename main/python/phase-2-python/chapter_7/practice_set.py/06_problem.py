1af4a41af87bc6dd9f29d33e63889b82545f54ac
adityasharma <mahiadit4321@gmail.com>
2026-06-21 22:15:18 +0530

    code formatting done

--- main/python/phase-2-python/chapter_7/practice_set.py/06_problem.py
+++ main/python/phase-2-python/chapter_7/practice_set.py/06_problem.py
@@ -1,25 +1,20 @@
-'''Write a program to calculate the factoral of a given number using for loop?'''
+"""Write a program to calculate the factoral of a given number using for loop?"""
 
 n = int(input("Enter the number: "))
 
-#5! = 1 X 2 X 3 X 4 X 5
+# 5! = 1 X 2 X 3 X 4 X 5
 product = 1
 
-for i in range(1, n+1):
+for i in range(1, n + 1):
     product = product * i
 
 print(f"The factorial of {n} is {product}")
 
 
-
-
-
-
 n = int(input("Enter the number: "))
 
 product = 1
 
-for i in range(1, n+1):
+for i in range(1, n + 1):
     product = product * i
 print(product)
-    
