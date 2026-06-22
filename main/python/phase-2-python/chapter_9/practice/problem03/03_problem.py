c6e13aea948fa12d0ba9b3fa7f9505a4d1ad050d
adityasharma <mahiadit4321@gmail.com>
2026-06-23 01:07:40 +0530

    Add multiplication table generator and output files for numbers 2 to 20

---
+++ main/python/phase-2-python/chapter_9/practice/problem03/03_problem.py
@@ -0,0 +1,14 @@
+"""write a program to generate multiplication tables frome 2 to 20 and wirte it to the different files. place these files in a folder for
+a 13-yrar old.
+"""  
+
+def  generatetable(n):
+	table = ""
+	for i in range(1, 11):
+		table += f'{n} X {i} = {n * i}\n'
+	with open(f"tables/table_{n}.txt", "w") as f:
+		f.write(table)
+
+
+for i in range(2, 21):
+	generatetable(i)
