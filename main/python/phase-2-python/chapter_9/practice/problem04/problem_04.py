f5ae30b39fa439cbd27591a391161359dde42d0f
adityasharma <mahiadit4321@gmail.com>
2026-06-23 01:34:17 +0530

    git replacing the contents of the word in files

---
+++ main/python/phase-2-python/chapter_9/practice/problem04/problem_04.py
@@ -0,0 +1,15 @@
+'''A file contains a word "donkey" multiple times. you need to weite a program which
+replace this word with ####3 by updating the same file.
+'''
+
+
+
+word = "Donkey"
+
+with open("file.txt", "r") as f:
+	data = f.read()
+
+New_data = data.replace(word, "######")
+
+with open("file.txt", "w") as f:
+	f.write(New_data)
