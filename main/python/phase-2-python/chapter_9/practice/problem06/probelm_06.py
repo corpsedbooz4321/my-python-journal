3cb2d15d1145e7f26f5b557c528dd5db64938b94
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-02 12:37:58 -0700

    refactor: adjust indentation for clarity in log file mining script

--- main/python/phase-2-python/chapter_9/practice/problem06/probelm_06.py
+++ main/python/phase-2-python/chapter_9/practice/problem06/probelm_06.py
@@ -9,12 +9,12 @@ file_path = os.path.join(script, "log.txt")
 if os.path.exists(file_path):
     with open(file_path, "r") as f:
          data = f.read()
-         #user = input('enter the word you want to find in the log file: ')
-         #user = str(user)
-         if "python" in data:
-            print(f'Found!')
-         else:
-            print('not found!')
+#user = input('enter the word you want to find in the log file: ')
+#user = str(user)
+if "python" in data:
+   print(f'Found!')
+else:
+   print('not found!')
 
 
 
