39f1d769b743bd5ed2c326e067f2d9d9b23e670d
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-03 09:13:27 -0700

    fix: improve comments and correct print statement in problem_09 script

--- main/python/phase-2-python/chapter_9/practice/problem09/problem_09.py
+++ main/python/phase-2-python/chapter_9/practice/problem09/problem_09.py
@@ -7,15 +7,15 @@ path_file = os.path.join(script, "1st.txt")
 another_file = os.path.join(script, "2nd.txt")
 
 
-if os.path.exists(path_file):
+if os.path.exists(path_file):#takes the content from the joined file "1st.txt"
     with open(path_file, "r") as f:
-        data1 = f.read()
+        data1 = f.read()#stores the sotres the content of the file 1st.txt in variable data1.
    
-if os.path.exists(another_file):
+#if os.path.exists(another_file):
     with open(another_file, "r") as f:
         data2 = f.read()
     
 if data1 == data2:
     print("file found with identical contents in it!")
 else:
-    print("Nothings wrong!")
+    print("Nothings wrong! files no files are found with same contents in it!")
