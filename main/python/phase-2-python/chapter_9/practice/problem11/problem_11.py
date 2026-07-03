6ed80e436d8eaee2e59a77cec2bc9d9392326518
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-03 10:03:19 -0700

    fix: correct variable names in problem_11 script for clarity

--- main/python/phase-2-python/chapter_9/practice/problem11/problem_11.py
+++ main/python/phase-2-python/chapter_9/practice/problem11/problem_11.py
@@ -1,15 +1,16 @@
-'''write a python program to  rename a file to "renamed_by_python.txt"'''
+'''write a python program to  rename a file to "renamed_by_python.txt" '''
 
 import os
 
 script = os.path.dirname(os.path.abspath(__file__))
 
-file = os.path.join(script, "log5.txt")
+file2 = os.path.join(script, "log5.txt")
+file1 = os.path.join(script, "renamed_by_python.txt")
 
 
 #if os.path.exists(file):
-with open(file, "r") as f:
+with open(file2, "r") as f:
     content = f.read()
-with open("renamed_by_python.txt", "w") as f:
+with open(file1, "w") as f:
     f.write(content)
   
