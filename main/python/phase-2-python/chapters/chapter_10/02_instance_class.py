23aa6336abf5e03243314d3e5ba3709fc21d8afd
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-11 15:18:00 +0530

    impoved code format

---
+++ main/python/phase-2-python/chapters/chapter_10/02_instance_class.py
@@ -0,0 +1,8 @@
+class Employee:
+    language = "Python"  # Class variable
+    salary= 50000  # Class variable
+
+
+aditya = Employee()
+aditya.language = "Java"  # Instance variable
+print(aditya.name, aditya.salary,aditya.language)  # Output: Java
