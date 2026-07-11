52c0494689b5aa225d6a5c5eed00af612a50f85a
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-11 11:58:31 +0530

    typo

--- main/python/phase-2-python/chapter's/chapter_10/01_class.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    name = "aditya"
-    lang = "py"
-    salary = 3999
-    
-aditya = Employee()
-aditya.name = "aditya"
-print(aditya.lang, aditya.salary)
---
+++ main/python/phase-2-python/chapters/chapter_10/01_class.py
@@ -0,0 +1,11 @@
+class Employee:
+    language = "Py"
+    salary = 1200000
+
+aditya = Employee()
+aditya.name = "aditya"
+print(aditya.language, aditya.salary)
+
+ankit = Employee()
+ankit.name = "ankit"
+print(ankit.name, ankit.language, ankit.salary)
