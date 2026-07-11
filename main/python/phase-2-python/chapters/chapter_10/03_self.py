d80aa1d405ca2d87fb8cdbc7dcac05b15fe451be
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-11 16:00:26 +0530

    compelted the OOP's

---
+++ main/python/phase-2-python/chapters/chapter_10/03_self.py
@@ -0,0 +1,16 @@
+class Employee:
+    language = "Python"  # Class variable
+    salary= 50000  # Class variable
+    
+
+    def getinfo(self):
+        print(f"The language is {self.language}. The salary is {self.salary}")
+    def greet(self):
+        print("good morning!")
+
+
+aditya = Employee()
+aditya.language = "Java"  # Instance variable
+print(aditya.salary,aditya.language)  # Output: Java
+aditya.getinfo()
+aditya.greet()
