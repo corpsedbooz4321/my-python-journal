f188b3ce894bb34e7e9e2429cd59d62d99aaf275
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-15 20:03:49 +0530

    added anotations

--- main/python/phase-2-python/chapters/chapter_10/03_self.py
+++ main/python/phase-2-python/chapters/chapter_10/03_self.py
@@ -1,16 +1,16 @@
 class Employee:
     language = "Python"  # Class variable
-    salary= 50000  # Class variable
-    
+    salary = 50000  # Class variable
 
     def getinfo(self):
         print(f"The language is {self.language}. The salary is {self.salary}")
+
     def greet(self):
         print("good morning!")
 
 
 aditya = Employee()
 aditya.language = "Java"  # Instance variable
-print(aditya.salary,aditya.language)  # Output: Java
+print(aditya.salary, aditya.language)  # Output: Java
 aditya.getinfo()
 aditya.greet()
--- main/python/phase-2-python/chapters/chapter_10/04_constructor.py
+++ main/python/phase-2-python/chapters/chapter_10/04_constructor.py
@@ -1,21 +1,24 @@
 class Employee:
     language = "Python"  # Class variable
-    salary= 50000  # Class variable
-    
-    def __init__(self, name, salary, language):#dunder method which is automatically called
+    salary = 50000  # Class variable
+
+    def __init__(
+        self, name, salary, language
+    ):  # dunder method which is automatically called
         self.name = name
         self.salary = salary
         self.language = language
         print("I am creating an object")
-					
+
     def getinfo(self):
         print(f"The language is {self.language}. The salary is {self.salary}")
+
     def greet(self):
         print("good morning!")
 
 
 aditya = Employee("aditya", 50000, "java")
 aditya.language = "Java"  # Instance variable
-#print(aditya.salary,aditya.language)  # Output: Java
+# print(aditya.salary,aditya.language)  # Output: Java
 aditya.getinfo()
 aditya.greet()
