791bdcbccdab3d377f5e2988985ae376e02255ab
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-18 00:08:08 +0530

    feat: implement Train class with methods for ticket booking, status, and fare retrieval

---
+++ main/python/phase-2-python/chapters/chapter_10/practice/05_problem.py
@@ -0,0 +1,20 @@
+"""Write a class train which has methods to book a ticket.
+get status(no. of seats) and get fare
+informatin of train running under indian Railways"""
+
+
+form random import randint
+
+
+
+class Trian:
+    def book(self, fro, to):
+        print(f'Ticket is booked in train no: {trainNo}
+        form {fro} to {to}")
+    
+    def getstatus(self, trainNo):
+        pass
+
+    def getfare(self, trainNo, fro, to):
+        print(f"Ticket fare is train no: {trianno} format
+        {fro} to {to} is {random")
