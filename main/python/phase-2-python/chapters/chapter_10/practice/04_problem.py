a5325e61073ee45ec459652a21b37d9644403606
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-17 23:51:30 +0530

    feat: implement calculator class with methods for square, cube, and square root calculations

---
+++ main/python/phase-2-python/chapters/chapter_10/practice/04_problem.py
@@ -0,0 +1,25 @@
+
+
+class calculator:
+    def __init__(self, n):
+        self.n = n
+
+    def square(self):
+        print(f"The square is {self.n*self.n}")
+
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
+    
+    def squareroot(self):
+        print(f"The squre root is {self.n**(1/2)}")
+
+    @staticmethod
+    def hello():
+        print("hello aditya!!")
+
+
+a = calculator(4)
+
+a.square()
+a.squareroot()
+a.cube()
