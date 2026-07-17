25fe8846412fd76e2ecdaa8d9877e6e48d23e4ba
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-17 23:36:25 +0530

    fix: correct instantiation of calculator class and ensure all methods are called

--- main/python/phase-2-python/chapters/chapter_10/practice/02_problem.py
+++ main/python/phase-2-python/chapters/chapter_10/practice/02_problem.py
@@ -8,6 +8,15 @@ class calculator:
     def square(self):
         print(f"The square is {self.n*self.n}")
 
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
 
-a = calculator()
+    def squareroot(self):
+        print(f"The squre root is {self.n**(1/2)}")
+
+
+a = calculator(4)
+
 a.square()
+a.squareroot()
+a.cube()
